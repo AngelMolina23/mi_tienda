4d16204d061d4111b1851624268f44c3954086e0
AngelMolina23 <125847601+AngelMolina23@users.noreply.github.com>
2026-06-02 18:38:31 -0600

    avance backend ecommerce dj

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,6 +139,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
